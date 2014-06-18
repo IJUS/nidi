555c4177fc9d49f18c7780404a95433cb7514676
pfried <philip.fried@ijus.net>
2014-06-18 17:09:25 -0400

    added automatic bootstrap to ContextHolder

--- src/main/groovy/net/ijus/nidi/ContextHolder.groovy
+++ src/main/groovy/net/ijus/nidi/ContextHolder.groovy
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 public class ContextHolder {
 	private static final Logger log = LoggerFactory.getLogger(ContextHolder)
 
-	private static Context ctx = new Context()
+	private static Context ctx = initContext()
 
 	public static ImplementationCache singletonCache = new BasicImplementationCache()
 	public static Set<Class> singletons = [] as Set
@@ -28,6 +28,16 @@ public class ContextHolder {
 		}
 		ctx = context
 
+	}
+
+	private static Context initContext() {
+		if (System.hasProperty(Configuration.CONFIG_PROPERTY_NAME)) {
+			log.info("Initializing nidi context from System property: ${System.getProperty(Configuration.CONFIG_PROPERTY_NAME)}")
+			return Configuration.configureNewFromSystemProperty()
+		} else {
+			log.debug("initializing empty nidi context")
+			return new Context()
+		}
 	}
 
 }
